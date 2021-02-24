5a4686c5254d92f5413325bcc52b4ca6695d754d
anhpt6852 <anhpt6852@seta-international.vn>
2021-02-24 13:26:59 +0700

    hotfix

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import './Login.css'
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  useEffect(() => setEmail(email), [email])
-  useEffect(() => setPassword(password), [password])
-
-  function handleSubmit() {
+  const handleSubmit = () => {
     console.log(email);
     console.log(password);
   }
 
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value)
+  }
+  
   return (
-      <form>
+      <form className = 'form-input'>
       <div>
-        <div>
+        <div className = 'username-input'>
         <label>
           Username:
-          <input type="text" name="name" value = {email} onChange = {(e) => setEmail(e.target.value)}/>
+          <input type="text" name="name" value = {email} onChange = {handleChangeEmail}/>
         </label>
         </div>
-        <div>
+        <div className = 'password-input'>
         <label>
           Password:
-          <input type="password" name="pass" value = {password} onChange = {(e) => setPassword(e.target.value)}/>
+          <input type="password" name="pass" value = {password} onChange = {handleChangePassword}/>
         </label>
         </div>
         <input type="submit" value="Submit" onClick = {handleSubmit}/></div>
