9d7ea83672863e2052f5f2c47eeeb1f6ebfee691
anhpt6852 <anhpt6852@seta-international.vn>
2021-02-24 14:38:32 +0700

    validate

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './Login.css'
 
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(false);
 
+  useEffect(() => {
+    if (email.length > 3 && password.length > 6) { setIsValid(!isValid)}
+  }, [email,password])
   const handleSubmit = () => {
-    console.log(email);
-    console.log(password);
+    console.log(email)
+    console.log(password)
   }
 
   const handleChangeEmail = (event) => {
@@ -23,16 +27,18 @@ function Login() {
         <div className = 'username-input'>
         <label>
           Username:
-          <input type="text" name="name" value = {email} onChange = {handleChangeEmail}/>
+          <input type="text" name="name" value = {email} onChange = {handleChangeEmail} />
         </label>
         </div>
+        <label className = 'error-message'> {(!isValid ? 'Wrong email' : '')}</label>
         <div className = 'password-input'>
         <label>
           Password:
           <input type="password" name="pass" value = {password} onChange = {handleChangePassword}/>
         </label>
         </div>
-        <input type="submit" value="Submit" onClick = {handleSubmit}/></div>
+        <label className = 'error-message'> {(!isValid ? 'Wrong password' : '')}</label>
+        <div><input type="submit" value="Submit" onClick = {handleSubmit} disabled = {!isValid}/></div></div>
       </form>
   );
 }
